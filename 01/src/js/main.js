74b6dcea90031509c11cceb161f63f94cbce3aee
Zsolt Meszaros <zsolt@sowad.com>
2017-03-01 10:17:49 +0100

    Add playSound() function

--- 01/src/js/main.js
+++ 01/src/js/main.js
@@ -1,16 +1,19 @@
-document.addEventListener('keydown', (e) => {
+const keys = document.querySelectorAll('.key');
+
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
   if (!audio || !key) { return; }
   audio.currentTime = 0;
   audio.play();
   key.classList.add('playing');
-});
+}
 
 function removeTransition(e) {
   if (e.propertyName !== 'transform') { return; }
   this.classList.remove('playing');
 }
 
-const keys = document.querySelectorAll('.key');
+document.addEventListener('keydown', playSound);
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
