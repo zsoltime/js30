68e1eefd7c22749edd6ea7991e1b96cd1ddc202e
Zsolt Meszaros <zsolt@sowad.com>
2017-03-05 10:17:29 +0100

    Add day 10

---
+++ 10/src/js/main.js
@@ -0,0 +1,19 @@
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+let lastChecked;
+
+function handleCheck(e) {
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      checkbox.checked = inBetween ? true : checkbox.checked;
+    });
+  }
+  lastChecked = this;
+}
+
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener('click', handleCheck);
+});
